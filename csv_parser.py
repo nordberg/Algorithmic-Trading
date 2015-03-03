f962ad45aba9a4fd82637c4706366fecc0d0e0f7
nordberg <marcusnordberg@live.se>
2015-03-03 20:57:21 +0100

    CSV parser

---
+++ csv_parser.py
@@ -0,0 +1,27 @@
+import csv
+import itertools
+
+def getHistData(filename):
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+
+        data = []
+        symbs = []
+        i = 0
+        for row in reader:
+            if i == 1:
+                for symb in row:
+                    symbs.append(symb)
+            day = {}
+
+            if i > 1:
+                #for k in range(0, len(row) - 1):
+                    #row[k] = row[k].split(',')
+                #row = list(itertools.chain(*row))
+                for j in range(0, len(symbs) - 1):
+                    day[symbs[j]] = row[j]
+                data.append(day)
+            i += 1
+    return data
+
+#print getHistData('ERIC-A.csv')
